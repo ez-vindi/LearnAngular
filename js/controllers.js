604cb59c0cd6689580bddf1f9a205ae1151508eb
Evandro Zanatta <ezanatta@tray.net.br>
2015-11-17 16:10:27 +0000

    Iniciando primeira funcao

---
+++ js/controllers.js
@@ -0,0 +1,11 @@
+/*global angular*/
+var blogApp = angular.module('blogApp', []);
+/*
+blogApp.controller('blogAppBody', function ($scope, $http) {
+  $http.get('http://jsonplaceholder.typicode.com/posts/').success(function(data) {
+      console.log(data);
+    //$scope.phones = data;
+  });
+
+  //$scope.orderProp = 'age';
+});*/
