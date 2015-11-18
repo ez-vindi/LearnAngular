5c397b8642978a6d95e35657ce27f1340d048e8c
Evandro Zanatta <ezanatta@tray.net.br>
2015-11-18 16:31:08 +0000

    Finalizando a funcao de obter a lista de usuarios

--- js/controllers.js
+++ js/controllers.js
@@ -1,20 +1,22 @@
-/*global angular*//*
-var blogApp = angular.module('blogApp', []);
-/*
-blogApp.controller('blogAppBody', function ($scope, $http) {
-  $http.get('http://jsonplaceholder.typicode.com/posts/').success(function(data) {
-      console.log(data);
-    //$scope.phones = data;
-  });
+/*global angular*/
 
-  //$scope.orderProp = 'age';
-});*/
-
+/* Obter postagens */
 var app = angular.module('myApp', []);
+
+/*
 app.controller('customersCtrl', function($scope, $http) {
   $http.get("http://jsonplaceholder.typicode.com/posts/")
   .success(function (response) {
     $scope.data = response;
+    
+  });
+});
+*/
+
+app.controller('showusers', function($scope, $http) {
+  $http.get("http://jsonplaceholder.typicode.com/users/")
+  .success(function (response) {
+    $scope.users = response;
     
   });
 });
