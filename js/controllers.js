54f260cf2c162289e48cf8c3ca4217e7c3119ec6
Evandro Zanatta <ezanatta@tray.net.br>
2015-11-18 16:17:13 +0000

    Terminando a funcao de obter dados de todos os artigos

--- js/controllers.js
+++ js/controllers.js
@@ -1,4 +1,4 @@
-/*global angular*/
+/*global angular*//*
 var blogApp = angular.module('blogApp', []);
 /*
 blogApp.controller('blogAppBody', function ($scope, $http) {
@@ -9,3 +9,12 @@ blogApp.controller('blogAppBody', function ($scope, $http) {
 
   //$scope.orderProp = 'age';
 });*/
+
+var app = angular.module('myApp', []);
+app.controller('customersCtrl', function($scope, $http) {
+  $http.get("http://jsonplaceholder.typicode.com/posts/")
+  .success(function (response) {
+    $scope.data = response;
+    
+  });
+});
